e4e0f253cba3c11aea58a21fbbcd7e16f1f4eb60
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-05-05 01:26:14 +0900

    fix devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'comics#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get 'sign_in', to: "users/sessions#new"
+    get 'sign_out', to: "users/sessions#destroy" 
+  end
   resources :comics, only: :index
+  resources :winpoints, only: :new
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
