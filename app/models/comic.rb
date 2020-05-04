73c3cda322f72c5ed1c1dfde37e161321958337e
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-05-05 01:30:36 +0900

    set default  value 0 to point
    winpoinテーブルのpointカラムにデフォルト値を設定

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,4 +1,13 @@
 class Comic < ApplicationRecord
+  after_initialize :set_default_values
+  # Use this if you wanna set default values only when creating a new record.
+  #after_initialize :set_default_values, if: :new_record?
+
+  private
+
+  def set_default_values
+    self.match        ||= 0
+  end
   has_many :comments
   has_many :winpoints
 end
