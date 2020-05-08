6ed2fbe746b88413b6bb7e14d50f01190fb892c7
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-05-08 16:31:17 +0900

    accept nested attributes for
    winpointテーブルのpointカラムとmatchテーブルのquantityカラムに同時にデータを入れるための記述

--- app/controllers/winpoints_controller.rb
+++ app/controllers/winpoints_controller.rb
@@ -1,5 +1,17 @@
 class WinpointsController < ApplicationController
   def new
     @comics = Comic.order(match: "ASC").limit(2)
+    @winpoint = Winpoint.new
+    @winpoint.matches.new
+  end
+
+  def create
+    @winpoint = Winpoint.new(point_params)
+  end
+
+  private
+
+  def point_params
+    params.require(:winpoint).permit(:point, matches_attributes:[:quantity])
   end
 end
--- app/models/winpoint.rb
+++ app/models/winpoint.rb
@@ -1,4 +1,7 @@
 class Winpoint < ApplicationRecord
   belongs_to :comic
   belongs_to :user
+  has_many :matches
+
+  accepts_nested_attributes_for :matches
 end
