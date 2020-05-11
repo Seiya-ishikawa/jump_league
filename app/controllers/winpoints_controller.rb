ed7bd7af59265938b6feecfb255ca5e7d20df5cd
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-05-11 23:59:39 +0900

    fix @comics
    @comicsの修正

--- app/controllers/winpoints_controller.rb
+++ app/controllers/winpoints_controller.rb
@@ -1,6 +1,10 @@
 class WinpointsController < ApplicationController
   def new
+    if Winpoint.point.conut == 20
     @comics = Comic.order(match: "ASC").limit(2)
+    else
+      @comics=Comic.RAND(2)
+    end
     @winpoint = Winpoint.new
     @winpoint.matches.new
   end
