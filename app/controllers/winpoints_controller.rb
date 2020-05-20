97f253447e322ed5a1b52a776250cde4cec31281
Seiya-ishikawa <s.akg1225@softbank.ne.jp>
2020-05-20 13:54:33 +0900

    add_before_acton
    before_actionでnewとcreateに同一の@comicsを渡す

--- app/controllers/winpoints_controller.rb
+++ app/controllers/winpoints_controller.rb
@@ -1,21 +1,30 @@
 class WinpointsController < ApplicationController
+  before_action :set_comic_id, only:[:new,:create]
   def new
-    if Winpoint.count == 20
-    @comics = Comic.order(match: "ASC").limit(2)
-    else
-      @comics=Comic.order("RAND()").limit(2)
-    end
     @winpoint = Winpoint.new
-    @winpoint.matches.new
+    @winpoint.matches.build
   end
 
   def create
     @winpoint = Winpoint.new(point_params)
+    if @winpoint.save
+    redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
+  def set_comic_id
+    if Winpoint.count > 20
+      @comics = Comic.order(match: "ASC").limit(2)
+      else
+        @comics=Comic.order("RAND()").limit(2)
+      end
+  end
+
   def point_params
-    params.require(:winpoint).permit(:point, matches_attributes:[:quantity])
+    params.require(:winpoint).permit(:point,:comic_id).merge(user_id: current_user.id)
   end
 end
